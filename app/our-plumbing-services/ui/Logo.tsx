b8dc576c3aa6a59b707febb90713c08bc66c829a
blessed <sibandablessed724@gmail.com>
2024-03-28 10:48:55 +0200

    Update Logo.tsx

--- app/our-plumbing-services/ui/Logo.tsx
+++ app/our-plumbing-services/ui/Logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div className="flex flex-row dark:bg-white bg-black dark:bg-dot-black/[0.2] bg-dot-white/[0.2] rounded-[2rem] md:px-[2rem] md:ml-[4rem]">
+    <div className="flex flex-row  md:px-[2rem] md:ml-[4rem]">
       <div className="container mx-auto h-[3rem] w-[3rem] md:h-[6rem] md:w-[6rem]">
         <Image
           src="/img/burgeon-logo.png" // the image file should be in the public folder
